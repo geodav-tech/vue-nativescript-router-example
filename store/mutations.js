718b30e321e98312aa40d8f6e5b6d3b066aa7145
Shawn Pacarar <spacarar@hotmail.com>
2019-03-25 11:21:47 -0600

    Update mutations.js
    logical error in taking off the last element

--- store/mutations.js
+++ store/mutations.js
@@ -3,7 +3,7 @@ import Vue from 'nativescript-vue'
 export default {
 	navigateBack(state) {
 		if(state.history.length) {
-			Vue.set(state, 'currentRoute', state.history.splice(0, -1))//take off last element and set current
+			Vue.set(state, 'currentRoute', state.history.splice(-1, 1))//take off last element and set current
 		}
 	},
 	navigateTo(state, newRoute, options) {
